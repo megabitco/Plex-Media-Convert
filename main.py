e184822004e8f69686a9b3861fea75e4c53224aa
Megabit Entertainment <>
2022-07-09 08:22:30 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import os
+
+print("Megabit Media Converter v2")
+
+convert_path="/home/megabitco/convert"
+local_path=os.path.join(convert_path,"local")
+tvshow_path=os.path.join(convert_path,"TV Shows")
+movie_path=os.path.join(convert_path, "Movies")
+
+# TV Conversion
+for 
